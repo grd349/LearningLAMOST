f8988371864df176e906ad28f4bc9e2658f2d6b3
Christopher Burch <cpb405@star.sr.bham.ac.uk>
2017-09-28 13:36:18 +0100

    Added Class

--- Chris/FITS.py
+++ Chris/FITS.py
@@ -1,21 +1,38 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import glob
 from astropy.io import fits
 
+class Spectrum:
+	def __init__(self,path):
+		hdulist = fits.open(path)
+
+		self.flux = (hdulist[0].data)[0]
+		self.date = hdulist[0].header["DATE"]
+		self.t_ID = hdulist[0].header["T_INFO"]
+		self.SNR = hdulist[0].header["SN_U"]
+		self.wavelength = np.linspace(3690,9000,len(self.flux))
+		hdulist.close()
+
+	def plotFlux(self):
+		plt.plot(self.wavelength,self.flux)
+		plt.xlabel("Wavelength [Angstroms]")
+		plt.ylabel("Flux")
+		plt.title("ID {}, SNR {}, Date {}".format(self.t_ID, self.SNR, self.date))
+		plt.show()
+
+spectra = []
+
 for fitsName in glob.glob('../Data/relearninglamost/*.fits'):
-    hdulist = fits.open(fitsName)
-    
-    dat = hdulist[0].data
 
-    date = hdulist[0].header["DATE"]
-    t_ID = hdulist[0].header["T_INFO"]
-    SNR = hdulist[0].header["SN_U"]
+	spectra.append(Spectrum(fitsName))
 
-    plt.plot(dat[0])
-    plt.xlabel("Wavelength [Angstroms]")
-    plt.ylabel("Flux")
-    plt.title("ID {}, SNR {}, Date {}".format(t_ID, SNR, date))
-    plt.show()
+for spectrumNumber in spectra:
+	spectrumNumber.plotFlux()
 
-    hdulist.close()
+
+	
+
+
+
 
