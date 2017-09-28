496a9f35ea6b0d56cf98d7ed0175c8befb140570
Matthew Scourfield <mrs493@xun0.sr.bham.ac.uk>
2017-09-28 13:37:28 +0100

    Adddedd class

--- Matt/fits.py
+++ Matt/fits.py
@@ -1,15 +1,29 @@
 from astropy.io import fits
+import scipy as sp
 import matplotlib.pyplot as plt
+import glob
 
-hdulist = fits.open('../Shared/spec-55862-B6212_sp08-147.fits')
+class Spectrum:
+	def __init__(self, path):
+		hdulist = fits.open(path)
+		self.flux = hdulist[0].data[0]
+		self.date = hdulist[0].header['DATE']
+		self.t_ID = hdulist[0].header['T_INFO']
+		self.SNR = hdulist[0].header['SN_U']
+		self.wavelength = sp.linspace(3690, 9100, len(self.flux))
+		hdulist.close()
+	def plotFlux(self):
+		plt.figure()
+		plt.plot(self.wavelength,self.flux)
+		plt.xlabel('Wavelength \ Angstroms')
+		plt.ylabel('Flux')
+		plt.title(self.t_ID)
+		plt.show()
 
-hdulist.info()
+spectra = []
 
-data = hdulist[0].data
+for fitsName in glob.glob('../Data/relearninglamost/*.fits'):
+	spectra.append(Spectrum(fitsName))
 
-plt.plot(data[1])
-
-plt.show()
-
-hdulist.close()
-
+for i in spectra:
+	i.plotFlux()
