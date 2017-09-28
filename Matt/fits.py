59c07185eb47129bc96413572d4093a76d7e523e
Matthew Scourfield <mrs493@xun0.sr.bham.ac.uk>
2017-09-28 14:51:58 +0100

    fixed issue #1

--- Matt/fits.py
+++ Matt/fits.py
@@ -10,9 +10,14 @@ class Spectrum:
         hdulist = fits.open(path)
         self.flux = hdulist[0].data[0]
         self.date = hdulist[0].header['DATE']
-        self.t_ID = hdulist[0].header['T_INFO']
+        self.SPID = hdulist[0].header['SPID']
         self.SNR = hdulist[0].header['SN_U']
-        self.wavelength = sp.linspace(3690, 9100, len(self.flux))
+	
+	init = hdulist[0].header['COEFF0']
+	disp = hdulist[0].header['COEFF1']
+
+	self.wavelength = 10**sp.arange(init, init+disp*len(self.flux), disp)[0:len(self.flux)]
+	
         hdulist.close()
 
     def plotFlux(self):
@@ -20,7 +25,7 @@ class Spectrum:
         ax.plot(self.wavelength,self.flux)
         ax.set_xlabel('Wavelength \ Angstroms')
         ax.set_ylabel('Flux')
-        ax.set_title(self.t_ID)
+        ax.set_title(self.SPID)
         ax.set_yscale('log')
         plt.show()
 
