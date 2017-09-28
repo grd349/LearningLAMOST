01ba514797dbff6de97a9187675d5aebe5b292f4
davies <grd349@gmail.com>
2017-09-28 13:59:06 +0100

    Changed plotting

--- Matt/fits.py
+++ Matt/fits.py
@@ -1,29 +1,33 @@
+#!/usr/bin/env python3
+
 from astropy.io import fits
 import scipy as sp
 import matplotlib.pyplot as plt
 import glob
 
 class Spectrum:
-	def __init__(self, path):
-		hdulist = fits.open(path)
-		self.flux = hdulist[0].data[0]
-		self.date = hdulist[0].header['DATE']
-		self.t_ID = hdulist[0].header['T_INFO']
-		self.SNR = hdulist[0].header['SN_U']
-		self.wavelength = sp.linspace(3690, 9100, len(self.flux))
-		hdulist.close()
-	def plotFlux(self):
-		plt.figure()
-		plt.plot(self.wavelength,self.flux)
-		plt.xlabel('Wavelength \ Angstroms')
-		plt.ylabel('Flux')
-		plt.title(self.t_ID)
-		plt.show()
+    def __init__(self, path):
+        hdulist = fits.open(path)
+        self.flux = hdulist[0].data[0]
+        self.date = hdulist[0].header['DATE']
+        self.t_ID = hdulist[0].header['T_INFO']
+        self.SNR = hdulist[0].header['SN_U']
+        self.wavelength = sp.linspace(3690, 9100, len(self.flux))
+        hdulist.close()
+
+    def plotFlux(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.wavelength,self.flux)
+        ax.set_xlabel('Wavelength \ Angstroms')
+        ax.set_ylabel('Flux')
+        ax.set_title(self.t_ID)
+        ax.set_yscale('log')
+        plt.show()
 
 spectra = []
 
 for fitsName in glob.glob('../Data/relearninglamost/*.fits'):
-	spectra.append(Spectrum(fitsName))
+    spectra.append(Spectrum(fitsName))
 
 for i in spectra:
-	i.plotFlux()
+    i.plotFlux()
