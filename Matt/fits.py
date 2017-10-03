95fce230b764c507aacfaa110fee585a17eb1edf
Matthew Scourfield <mrs493@star.sr.bham.ac.uk>
2017-10-03 15:43:01 +0100

    Added normalization to blackbody curves

--- Matt/fits.py
+++ Matt/fits.py
@@ -17,17 +17,33 @@ class Spectrum:
         self.SNR_I = hdulist[0].header['SN_I']
         self.SNR_Z = hdulist[0].header['SN_Z']
         self.CLASS = hdulist[0].header['CLASS']
+        
+        self.totCounts = sp.sum(self.flux)
         	
         init = hdulist[0].header['COEFF0']
         disp = hdulist[0].header['COEFF1']
         
         self.wavelength = 10**sp.arange(init, init+disp*(len(self.flux)-0.9), disp)
+        
+        h = 6.63e-34
+        c = 3e8
+        k = 1.38e-23
+        T = 7000
+        E = (8*sp.pi*h*c)/((self.wavelength*1e-10)**5*(sp.exp(h*c/((self.wavelength*1e-10)*k*T))-1))
+        
+        fudge = self.totCounts/sp.sum(E)
+        
+        self.bbc = fudge*E
         	
         hdulist.close()
     
     def plotFlux(self, element = None):
+
+        
         fig, ax = plt.subplots()
         ax.plot(self.wavelength,self.flux)
+        ax.plot(self.wavelength,self.bbc)
+        
         ax.set_xlabel('Wavelength \ Angstroms')
         ax.set_ylabel('Flux')
         ax.set_yscale('log')
@@ -48,9 +64,10 @@ class Spectrum:
         plt.show()
 
 spectra = []
+stars = 0
 
-for fitsName in glob.glob('../Data/relearninglamost/*.fits'):
+for fitsName in glob.glob('../Data/DR1/*.fits'):
     spectra.append(Spectrum(fitsName))
 
 for i in spectra:
-    i.plotFlux('Iron')
+    i.plotFlux()
