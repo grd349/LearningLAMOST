bda34febafeea655f1c5649f20abd9fd2ec37c66
davies <grd349@gmail.com>
2017-10-12 12:53:53 +0100

    Added pandas examples

--- read_data.py
+++ read_data.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn
 
 if __name__ == "__main__":
     sfile = 'Data/dr3_stellar.csv'
@@ -10,6 +11,24 @@ if __name__ == "__main__":
     df = pd.read_csv(sfile, sep='|')
     print(df.columns)
 
+    print(df.loc[0:10].obsid)
+
+    print(df.loc[df.obsid == 101005].teff)
+
+    # Panadas make a dataframe ...
+
+    df_spectra = pd.DataFrame(columns=['obsid', 'feature'])
+    ids = [(101001, 3), (101005, 4), (101008, 10)]
+    for i in ids:
+        df_spectra.loc[len(df_spectra)] = [i[0], i[1]]
+
+    print(df_spectra)
+    df = df.merge(df_spectra, on='obsid', how='inner')
+
+    print(df)
+
+    df.to_csv('my_data.csv')
+    
     fig, ax = plt.subplots()
     ax.hist(df.teff)
     plt.show()
