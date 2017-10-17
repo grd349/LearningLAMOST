b83f084dd853c00c9df6887ac5db767cfae5c7a9
Matthew Scourfield <mrs493@star.sr.bham.ac.uk>
2017-10-17 17:37:24 +0100

    fixed bands and changed plots for blackbody curves

--- Matt/blackbodyRF.py
+++ Matt/blackbodyRF.py
@@ -2,6 +2,7 @@ from astropy.io import fits
 import scipy as sp
 import matplotlib.pyplot as plt
 import glob
+import seaborn as sns
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
@@ -15,15 +16,15 @@ def blackbody(T):
     k = 1.38e-23
     return (8*sp.pi*h*c)/((wavelength*1e-10)**5*(sp.exp(h*c/((wavelength*1e-10)*k*T))-1))
 
-temp = sp.array(sp.random.normal(6000, 1000, 200))
+temp = sp.array(sp.random.normal(6000, 2000, 800))
 
 colour = []
 
 for t in temp:
     spectra = blackbody(t)
-    B = sp.sum(spectra[490:960])
-    V = sp.sum(spectra[1035:1475])
-    colour.append(sp.log10(V/B))
+    B = -2.5*sp.log10(sp.sum(spectra[490:960]))
+    V = -2.5*sp.log10(sp.sum(spectra[1035:1475]))
+    colour.append(B - V)
 
 colour = sp.reshape(colour, (-1, 1))
 
@@ -32,16 +33,48 @@ for i in range(len(temp)):
 
 fig, ax = plt.subplots()
 ax.scatter(colour, temp)
-ax.set_xlabel('Colour / log(V/B)')
+ax.set_xlabel('Colour Feature / B - V')
 ax.set_ylabel('Temperature \ K')
-ax.set_title('Colour vs. Temperature ')
+ax.set_title('Colour Feature vs. Temperature')
+plt.show()
 
 clf = RandomForestClassifier()
 
-"""
-Need to add in cross validation method
-"""
+kf = cross_validation.KFold(n = len(colour), n_folds = 5, shuffle = True)
 
+for train_index, test_index in kf:
+
+	X_train, X_test = colour[train_index], colour[test_index]
+	y_train, y_test = temp[train_index], temp[test_index]
+	clf = clf.fit(X_train, y_train)
+	test_pred = clf.predict(X_test)
+
+	fig, ax = plt.subplots(2,2)
+	fig.canvas.set_window_title('Random Forest Temperature Model for Black Body Curves')
+
+	ax[0][0].scatter(y_test, test_pred)
+	ax[0][0].set_xlabel('Actual temperature \ K')
+	ax[0][0].set_ylabel('Predicted temperature \ K')
+	ax[0][0].set_title('Actual vs. Predicted temperature')
+
+	error = test_pred - y_test
+
+	sns.kdeplot(error, ax=ax[0][1], shade=True)
+	ax[0][1].set_title('Error of Prediction')
+	ax[0][1].set_xlabel('Absolute Error')
+	ax[0][1].set_ylabel('Number')
+	ax[0][1].set_title('Absolute Error on Temperature Prediction')
+
+	sns.residplot(y_test, test_pred, lowess = True, ax = ax[1][0])
+	ax[1][0].set_title('Residuals of Prediction')
+	ax[1][0].set_xlabel('Actual Temperature \ K')
+	ax[1][0].set_ylabel('Predicted Temperature Residual \ K')
+	ax[1][0].set_title('Actual vs. Prediction Residual Temperature')
+
+	plt.tight_layout()
+
+plt.show()
+'''
 clf.fit(colour, temp)
 
 pred = clf.predict(colour)
@@ -55,34 +88,7 @@ ax.set_ylabel('Predicted temperature \ K')
 ax.set_title('Actual vs. Predicted temperature')
 
 fig, ax = plt.subplots()
-ax.hist(error)
+sns.kdeplot(error, ax=ax, shade=True)
 ax.set_title('Error of Prediction')
 plt.show()
-
-"""
-spectra = Spectra('../Data/DR1/*.fits')
-    
-colour = sp.reshape(spectra.colour, (-1, 1)) #input data needs each sample in a separate row, not column
-                   
-temperature = sp.array([int(i.flux[100]) for i in spectra.spectra]) #need to find actual temperatures, currently using flux as a placeholder
-                      
-#need samples and their 'correct values' in scipy arrays, so can index them using kfolds
-    
-clf = RandomForestClassifier()
-kf = cross_validation.KFold(n=len(colour), n_folds=5, shuffle=True)
-
-error = 0
-
-for train_index, test_index in kf:   
-    X_train, X_test = colour[train_index], colour[test_index]
-    y_train, y_test = temperature[train_index], temperature[test_index]
-    clf = clf.fit(X_test,y_test)
-    test_pred = clf.predict(X_test)
-    error += sp.sum(abs(test_pred-y_test)/len(colour))#need to make more mathematical test of accuracy, placeholder
-    
-clf.fit(colour, temperature)
-
-plt.scatter(colour, clf.predict(colour) - temperature)
-
-plt.show()
-"""
+'''
