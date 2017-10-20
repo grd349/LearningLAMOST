6dab690dccac1bf5371fe24ec6df6483f685dfb9
Matthew Scourfield <mrs493@star.sr.bham.ac.uk>
2017-10-20 15:00:35 +0100

    coppied plots from bb to specrta RF code

--- Matt/read_data.py
+++ Matt/read_data.py
@@ -20,7 +20,7 @@ dr1 = pd.DataFrame(columns = ['designation', 'flux', 'wavelength', 'totalCounts'
 
 letters = {'B':[3980, 4920], 'V':[5070, 5950], 'R':[5890, 7270]}
 
-for fitsName in glob.glob('../Data/DR1/*.fits'):
+for fitsName in glob.glob('/data2/mrs493/DR1/*.fits'):
     hdulist = fits.open(fitsName)
     
     init = hdulist[0].header['COEFF0']
@@ -35,122 +35,13 @@ for fitsName in glob.glob('../Data/DR1/*.fits'):
     for letter in letters:
         lower = sp.searchsorted(wavelength, letters[letter][0], side = 'left')
         upper = sp.searchsorted(wavelength, letters[letter][1], side = 'right')
-        bands[letter] = sp.sum(flux[lower:upper])
+        bands[letter] = -2.5*sp.log10(sp.sum(flux[lower:upper]))
         
-    colour = sp.log10(bands['B']/bands['V'])
+    colour = bands['B'] - bands['V']
     
     dr1.loc[len(dr1)] = [hdulist[0].header['DESIG'][7:], flux, wavelength, totalCounts, bands['B'], bands['V'], bands['R'], colour]
     hdulist.close()
 
 df = catalog.merge(dr1, on='designation', how='inner')
 
-
-"""
-change above to a function, or write to a csv to be used in future code
-"""
-
-
-fig, ax = plt.subplots()
-ax.scatter(df.colour, df.teff)
-ax.set_title('Star colour vs. temperature')
-ax.set_xlabel('Star colour / log(B/V)')
-ax.set_ylabel('Temperature / K')
-
-plt.savefig('cvt')
-
-#plot colour vs. temp
-
-colour = sp.reshape(df.colour, (-1, 1))
-temperature = []
-
-for i in range(len(df.teff)):
-    temperature.append(int(df.teff[i]))
-        #the temperatures need to be integers
-
-temperature= sp.array(temperature)
-
-clf = RandomForestClassifier()
-kf = cross_validation.KFold(n=len(colour), n_folds=5, shuffle=True)
-
-accuracy = []
-
-for j in range(1,21):
-
-	accuracy_sum = []
-	kf = cross_validation.KFold(n = len(colour), n_folds = 5, shuffle = True)
-
-	for train_index, test_index in kf:
-	
-		X_train, X_test = colour[train_index], colour[test_index]
-		y_train, y_test = temperature[train_index], temperature[test_index]
-		clf = clf.fit(X_train, y_train)
-		test_pred = clf.predict(X_test)
-		accuracy = sp.concatenate((abs(test_pred - y_test)/(y_test*1.0), accuracy))
-		
-mean_accuracy = sp.mean(accuracy)
-std_accuracy = sp.std(accuracy)
-
-clf.fit(colour, temperature)
-pred = clf.predict(colour)
-
-error = pred - temperature
-
-fig, ax = plt.subplots()
-ax.scatter(temperature, pred)
-ax.set_xlabel('Actual temperature \ K')
-ax.set_ylabel('Predicted temperature \ K')
-ax.set_title('Actual vs. Predicted temperature')
-
-ax.text(10000, 4000, '{} +/- {}'.format(mean_accuracy, std_accuracy), size = 15, ha = 'right')
-
-plt.savefig('fit')
-
-#plot actual vs. model temp
-
-
-sns.residplot(temperature, pred, lowess=True)
-
-plt.savefig('residue')
-
-for i in range(len(pred)):
-	if (abs(pred[i] - temperature[i])/(temperature[i]*1.0)) > 0.1:
-		plt.figure()
-		plt.plot(df.loc[i].wavelength, df.loc[i].flux, color = 'k')
-		plt.axvline(3980)
-		plt.axvline(4920)
-		plt.axvline(5070)
-		plt.axvline(5950)
-		plt.savefig('spectra' + str(i))
-
-"""
-if __name__ == "__main__":
-    sfile = '/data2/mrs493/dr1_stellar.csv'
-
-    df = pd.read_csv(sfile, sep='|')
-    print(df.columns)
-    
-    print(df.loc[0:10].obsid)
-
-    print(df.loc[df.obsid == 101005].teff)
-
-    # Panadas make a dataframe ...
-
-    df_spectra = pd.DataFrame(columns=['obsid', 'feature'])
-    ids = [(101001, 3), (101005, 4), (101008, 10)]
-    for i in ids:
-        df_spectra.loc[len(df_spectra)] = [i[0], i[1]]
-
-    print(df_spectra)
-    df = df.merge(df_spectra, on='obsid', how='inner')
-
-    print(df)
-
-    df.to_csv('my_data.csv')
-    
-    fig, ax = plt.subplots()
-    ax.hist(df.teff)
-    plt.show()
-    fig, ax = plt.subplots()
-    ax.hist(df['feh'])
-    plt.show()
-"""
+df.to_csv('/data2/mrs493/my_data.csv')
--- Matt/spectraRF.py
+++ Matt/spectraRF.py
@@ -1,41 +1,23 @@
-from astropy.io import fits
+#!/usr/bin/env python3
+
+import pandas as pd
 import scipy as sp
 import matplotlib.pyplot as plt
 import glob
+from astropy.io import fits
 import seaborn as sns
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
 
-#from fits import Spectra
+sfile = '/data2/mrs493/my_data.csv'
 
-def blackbody(T):
-    wavelength = sp.linspace(3000, 9000, 3001)
-    h = 6.63e-34
-    c = 3e8
-    k = 1.38e-23
-    return (8*sp.pi*h*c)/((wavelength*1e-10)**5*(sp.exp(h*c/((wavelength*1e-10)*k*T))-1))
-	#calculate a blackboduy curve for a given temperature
+df = pd.read_csv(sfile, sep=',')
 
-temp = sp.array(sp.random.normal(6000, 2000, 800))
-	#generate a normal distribution of tempratures
-
-for i in range(len(temp)):
-    temp[i] = int(int(temp[i]))
-	#convert the temperatures to (positive) integers for use in the algorithm
-
-colour = []
-
-for t in temp:
-    spectra = blackbody(t)
-    B = -2.5*sp.log10(sp.sum(spectra[490:960]))
-    V = -2.5*sp.log10(sp.sum(spectra[1035:1475]))
-    colour.append(B - V)
-	#for each temperature, generate a balckbody curve then calculate its colour features
-
-colour = sp.reshape(colour, (-1, 1))
+colour = sp.reshape(df.colour, (-1, 1))
 	#reshape the colour to a column vector for use in the algorithm
 
+temp = sp.array([int(i) for i in df.teff])
 
 fig, ax = plt.subplots()
 ax.scatter(colour, temp)#, c = temp)
@@ -102,3 +84,80 @@ for train_index, test_index in kf:
 	plt.tight_layout()
 
 plt.show()
+
+'''
+
+fig, ax = plt.subplots()
+ax.scatter(df.colour, df.teff)
+ax.set_title('Star colour vs. temperature')
+ax.set_xlabel('Star colour / log(B/V)')
+ax.set_ylabel('Temperature / K')
+
+#plt.savefig('cvt')
+
+#plot colour vs. temp
+
+colour = sp.reshape(df.colour, (-1, 1))
+temperature = []
+
+for i in range(len(df.teff)):
+    temperature.append(int(df.teff[i]))
+        #the temperatures need to be integers
+
+temperature= sp.array(temperature)
+
+clf = RandomForestClassifier()
+kf = cross_validation.KFold(n=len(colour), n_folds=5, shuffle=True)
+
+accuracy = []
+
+for j in range(1,21):
+
+	accuracy_sum = []
+	kf = cross_validation.KFold(n = len(colour), n_folds = 5, shuffle = True)
+
+	for train_index, test_index in kf:
+	
+		X_train, X_test = colour[train_index], colour[test_index]
+		y_train, y_test = temperature[train_index], temperature[test_index]
+		clf = clf.fit(X_train, y_train)
+		test_pred = clf.predict(X_test)
+		accuracy = sp.concatenate((abs(test_pred - y_test)/(y_test*1.0), accuracy))
+		
+mean_accuracy = sp.mean(accuracy)
+std_accuracy = sp.std(accuracy)
+
+clf.fit(colour, temperature)
+pred = clf.predict(colour)
+
+error = pred - temperature
+
+fig, ax = plt.subplots()
+ax.scatter(temperature, pred)
+ax.set_xlabel('Actual temperature \ K')
+ax.set_ylabel('Predicted temperature \ K')
+ax.set_title('Actual vs. Predicted temperature')
+
+ax.text(10000, 4000, '{} +/- {}'.format(mean_accuracy, std_accuracy), size = 15, ha = 'right')
+
+#plt.savefig('fit')
+
+#plot actual vs. model temp
+
+
+sns.residplot(temperature, pred, lowess=True)
+
+#plt.savefig('residue')
+
+for i in range(len(pred)):
+	if (abs(pred[i] - temperature[i])/(temperature[i]*1.0)) > 0.1:
+		plt.figure()
+		plt.plot(df.loc[i].wavelength, df.loc[i].flux, color = 'k')
+		plt.axvline(3980)
+		plt.axvline(4920)
+		plt.axvline(5070)
+		plt.axvline(5950)
+		#plt.savefig('spectra' + str(i))
+
+plt.show()
+'''
