f0370b78e8c12093a907ba9bd1060c0a7b38899a
Christopher Burch <cpb405@star.sr.bham.ac.uk>
2018-01-22 17:31:03 +0000

    Added logg and feh features

--- Chris/temperature_regressor.py
+++ Chris/temperature_regressor.py
@@ -19,60 +19,57 @@ class Temperature_Regressor():
         catalog = pd.read_csv(catalog_file, sep='|')
         catalog.drop_duplicates(subset = 'designation', inplace = True)
         self.df = catalog.merge(features_df, on='designation', how='inner')
+        self.names = self.df.columns[38:52]
         imputer = Imputer(missing_values = 0)
-        self.df[self.df.columns[38:52]] = imputer.fit_transform(self.df[self.df.columns[38:52]])
+        self.df[self.names] = imputer.fit_transform(self.df[self.names])
 
     def tune_hyperparameters(self, feats, temps, verbose=False):
         ''' Uses an exhaustive grid search to tune the hyperparameters of a random forest regressor '''
-        parameter_grid = [{'n_estimators':[1,10,20,40,60,80,100],'max_depth':[1,10,100,1000],'max_features':[3,5,8,10]}]
+        parameter_grid = [{'n_estimators':[20,40,60,80,100],'max_depth':[1000,2000,3000,4000,5000],'max_features':[6,9,12,15]}]
         regr = GridSearchCV(RandomForestRegressor(), parameter_grid, cv=5)
         regr.fit(feats, temps)
         if verbose:
             print(regr.best_params_)
         return regr.best_params_
     
-    def extract_features(self, df):
+    def extract_features(self):
         ''' Carries out feature engineering on the photometric and equivalent width features '''
-        self.names = df.columns[38:52]
-        features = df.as_matrix(columns = self.names)
-        self.names = [name[1:] for name in self.names]
+        features = self.df.as_matrix(columns = self.names)         
         col_features = features[:,1:5]
         col_names = self.names[1:5]
         for idx in range(len(col_features.T)-1):
             for j in np.arange(idx+1, len(col_features.T)):
                 d = np.reshape(col_features[:,idx] - col_features[:,j],(-1, 1))
-                features = np.hstack((features,d))
-            self.names = np.append(self.names,[col_names[idx] + "-" + i for i in col_names[idx+1:]])
+                n = col_names[idx][1:] + "-" + col_names[j][1:]
+                self.df[n] = d
+            self.names = np.append(self.names,[col_names[idx][1:] + "-" + i[1:] for i in col_names[idx+1:]])                     
         lin_features = features[:,5:14]
         lin_names = self.names[5:14]
         for idx in range(len(lin_features.T)-1):
             for j in np.arange(idx+1, len(lin_features.T)):
                 d = np.reshape(lin_features[:,idx] / lin_features[:,j],(-1,1))
-                features = np.hstack((features,d))
-            self.names = np.append(self.names,[lin_names[idx] + "/" + i for i in lin_names[idx+1:]])
-        return features
+                n = lin_names[idx][1:] + "/" + lin_names[j][1:]
+                self.df[n] = d
+            self.names = np.append(self.names,[lin_names[idx][1:] + "/" + i[1:] for i in lin_names[idx+1:]])
+        scaler = StandardScaler()
+        self.df[self.names] = scaler.fit_transform(self.df[self.names])
                                  
-    def predict_temperatures(self, tune=False, verbose=False):
+    def predict_temperatures(self, model, tune=False, verbose=False):
         ''' Fits a regressor to the data and returns model predictions and true values of a test set '''
+        train_df, self.test_df = train_test_split(self.df,test_size=0.5)
+        print(self.names)
+        X_train = train_df.as_matrix(columns = self.names)
+        X_test = self.test_df.as_matrix(columns = self.names)
+        y_train = train_df[model].values
+        self.y_test = self.test_df[model].values 
         if tune == True:
-            self.names = self.df.columns[38:52]
-            feats = self.df.as_matrix(columns = self.names)
-            temps = self.df['teff'].values
-            regr=RandomForestRegressor(self.tune_hyperparameters(feats,temps,True))
+            hyp = self.tune_hyperparameters(X_train,y_train,True)
+            self.max_features = hyp['max_features']
+            regr = RandomForestRegressor(n_estimators = hyp['n_estimators'], max_depth = hyp['max_depth'], max_features = self.max_features)   
         else:
             self.max_features = 12
-            regr=RandomForestRegressor(n_estimators=80, max_depth=10, max_features=self.max_features)
-        train_df, self.test_df = train_test_split(self.df,test_size=0.5)    
-        X_train = self.extract_features(train_df)
-        X_test = self.extract_features(self.test_df)
-        y_train = train_df['teff'].values
-        self.y_test = self.test_df['teff'].values
-        
-        scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test=scaler.transform(X_test)
-        
-        regr = regr.fit(X_train,y_train)
+            regr = RandomForestRegressor(n_estimators=60, max_depth=3000, max_features=self.max_features)     
+        regr = regr.fit(X_train,y_train)        
         self.y_test_pred = regr.predict(X_test)
         self.error = self.y_test_pred - self.y_test
         self.importances = regr.feature_importances_
@@ -90,10 +87,10 @@ class Temperature_Regressor():
         normalise = counts/np.sum(E)
         return normalise*E
 
-    def plot_results(self, regr='Random Forest Regressor'):
+    def plot_results(self, model, regr='Random Forest Regressor'):
         ''' Creates a 2 by 2 grid of subplots presenting the predictions of the RFR '''
         fig, ax = plt.subplots(2,2)
-        fig.suptitle(regr,y=1.03,fontsize=18)
+        fig.suptitle(regr + '\n' + model,y=1.03,fontsize=18)
         
         ends = [np.amin(self.y_test), np.amax(self.y_test)]
         ''' Predicted vs. actual temperature '''
@@ -110,7 +107,12 @@ class Temperature_Regressor():
         ax[0][1].set_ylabel('Fraction of Points with Error')
         ax[0][1].set_title('KDE Plot for Absolute Errors')
         
-        imp = [[i,n] for i,n in sorted(zip(self.importances, self.names), reverse = True)]        
+        names = self.names.copy()
+        for i in range(len(names)):
+            if names[i][0] == 'c' or names[i][0] == 'l':
+                names[i] = names[i][1:]
+                
+        imp = [[i,n] for i,n in sorted(zip(self.importances, names), reverse = True)]        
         ''' Importances bar plot '''
         sns.barplot([i[1] for i in imp][:self.max_features], [i[0] for i in imp][:self.max_features], ax = ax[1][0])
         ax[1][0].set_xlabel('Feature')
@@ -123,13 +125,14 @@ class Temperature_Regressor():
         desig_test = self.test_df['designation'].values
         outlier_row = self.test_df.loc[self.test_df['designation']==desig_test[outlier_index]].index[0]
         outlier = Spectrum('/data2/mrs493/DR1_3/' + self.test_df.get_value(outlier_row,'FILENAME'))      
-        blackbody_true = self.blackbody(self.test_df.get_value(outlier_row,'teff'), outlier.wavelength, np.nansum(outlier.flux))
-        blackbody_pred = self.blackbody(self.y_test_pred[outlier_index], outlier.wavelength, np.nansum(outlier.flux))
         
         ''' Outlier spectrum plot '''
         ax[1][1].plot(outlier.wavelength, outlier.flux)
-        ax[1][1].plot(outlier.wavelength,blackbody_true,'--',c='r',label='True Temp.')
-        ax[1][1].plot(outlier.wavelength,blackbody_pred,'--',c='b',label='Predicted Temp.')
+        if model == 'teff':
+            blackbody_true = self.blackbody(self.test_df.get_value(outlier_row,'teff'), outlier.wavelength, np.nansum(outlier.flux))
+            blackbody_pred = self.blackbody(self.y_test_pred[outlier_index], outlier.wavelength, np.nansum(outlier.flux))
+            ax[1][1].plot(outlier.wavelength,blackbody_true,'--',c='r',label='True Temp.')
+            ax[1][1].plot(outlier.wavelength,blackbody_pred,'--',c='b',label='Predicted Temp.')
         ax[1][1].set_xlabel('Wavelength / Angstroms')
         ax[1][1].set_ylabel('Flux')
         ax[1][1].set_title('Spectrum for Greatest Outlier')
@@ -139,7 +142,10 @@ class Temperature_Regressor():
         plt.show()
         	
 if __name__ == "__main__":
-    direc = 'TempCSVs2'
+    direc = 'TempCSVs1'
     spec_regr = Temperature_Regressor(direc)
-    spec_regr.predict_temperatures()
-    spec_regr.plot_results()
+    spec_regr.extract_features()
+    models = ['teff', 'logg', 'feh']
+    for mod in models:
+        spec_regr.predict_temperatures(tune = False, model = mod)
+        spec_regr.plot_results(model = mod)
