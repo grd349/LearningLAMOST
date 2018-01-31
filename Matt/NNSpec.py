8965373d12514de631cd5956e472bc8ac0b27f51
Matthew Scourfield <mrs493@star.sr.bham.ac.uk>
2018-01-31 14:15:26 +0000

    prelim neural net made and working

--- Matt/NNSpec.py
+++ Matt/NNSpec.py
@@ -1,9 +1,11 @@
 import glob
 from astropy.io import fits
-import numpy as sp
+import numpy as np
 import tensorflow as tf
 
-files = glob.glob('/data2/mrs493/DR1_2/*.fits')
+import time    
+
+files = glob.glob('/data2/mrs493/DR1_3/*.fits')
 
 classes = ['STAR', 'GALAXY', 'QSO', 'Unknown']
 
@@ -12,8 +14,13 @@ cls = len(classes)
 flux = []
 CLASS = []
 
-batch = 100
+#batchSize = 100
 wavelengths = 3800
+
+batch_frac= 0.2
+train_frac = 0.7
+
+t = time.time()
 
 for idx, file in enumerate(files):
     with fits.open(file) as hdulist:
@@ -24,21 +31,24 @@ for idx, file in enumerate(files):
     CLASS.append([0]*cls)
     CLASS[-1][classes.index(CLS)] = 1
 
-'''
+flux = np.array(flux)
+CLASS = np.array(CLASS)
+
+print(time.time() - t)
+
 for i in range(cls):
-    print(classes[i], ': ', sp.sum([x[i] for x in CLASS]))
-'''
+    print(classes[i], ': ', np.sum([x[i] for x in CLASS]))
 
-split = 3000
 
-x_train = sp.array(flux[:split])
-x_test = sp.array(flux[split:])
+split = np.random.randn(len(files))>=train_frac
 
-y_train = sp.array(CLASS[:split])
-y_test = sp.array(CLASS[split:])
+print(split)
 
-print([len(x) for x in x_train])
-print(y_train)
+x_train = np.array(flux[split])
+x_test = np.array(flux[~split])
+
+y_train = np.array(CLASS[split])
+y_test = np.array(CLASS[~split])
 
 '''need better train test split (sklearn?) and batching'''
 
@@ -53,20 +63,32 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-sess = tf.InteractiveSession()
-
-tf.global_variables_initializer().run()
-
-print('a')
-
-for _ in range(10):
-    batch = sp.random.choice(len(x_train), size = batch)
-    batch_x = x_train[batch]
-    batch_y = y_train[batch]
-    sess.run(train_step, feed_dict={x: x_train, y_: y_train})
-    '''change batching process'''
-
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={x: x_test, y_: y_test}))
-'''change to use test data'''
+
+accuracies = []
+
+confusion = tf.confusion_matrix(tf.argmax(y,1), tf.argmax(y_,1))
+
+with tf.Session() as sess:
+    t = time.time()
+    sess.run(tf.global_variables_initializer())
+    for i in range(20000):
+        #batch = np.random.choice(split, size = batchSize)
+        batch = np.random.randn(len(x_train))>=batch_frac
+        batch_x = x_train[batch]
+        batch_y = y_train[batch]
+        if i%100 ==0:
+            train_accuracy = sess.run(accuracy, feed_dict={x: x_test, y_: y_test})
+            print('step {} training accuracy {}'.format(i, train_accuracy))
+            accuracies.append(train_accuracy)
+        sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
+
+    conf, acc = sess.run([confusion, accuracy], feed_dict={x: x_test, y_: y_test})
+    print('test accuracy {}'.format(acc))
+    print(conf)
+    np.savetxt('confusion.csv', conf, fmt = '%i', delimiter = ',')
+    print(time.time() - t)
+    
+    '''add plots'''
+    
---
+++ Matt/plotNN.py
@@ -0,0 +1,23 @@
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import matplotlib.pyplot as plt
+#import seaborn as sb
+
+
+conf = np.loadtxt('confusion.csv', delimiter = ',')
+
+classes = ['STAR', 'GALAXY', 'QSO', 'Unknown']
+
+actual = [[row[i] for row in conf] for i in range(len(classes))]
+
+a = ['Matt', 'Chris', 'Chris', 'Matt', 'Chris']
+
+hot = LabelEncoder()
+
+print(hot.fit_transform(a))
+
+#actual = actual.reshape
+
+print(actual)
+
+ax, fig = plt.subplots()
