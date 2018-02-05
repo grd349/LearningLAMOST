308158cda858bcfd7fec48fa3db753bf305d2697
Christopher Burch <cpb405@star.sr.bham.ac.uk>
2018-02-05 17:09:55 +0000

    Continued work on neural net

--- Chris/Learning_TensorFlow/test_neural_net.py
+++ Chris/Learning_TensorFlow/test_neural_net.py
@@ -1,56 +1,66 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
 class Neural_Net():
     def __init__(self):
         nStars = 1000
         nGalaxies = 1000       
-        stars = []
-        galaxies = []
+        fStar = []
+        cStar = []
+        fGalaxy = []
+        cGalaxy = []
         temps = np.random.uniform(3000,10000,nStars)
         wavelength = np.linspace(4000,9000,1000)
         for idx in range(len(temps)):
-            stars.append(self.blackbody(temps[idx], wavelength))
+            fStar.append(self.blackbody(temps[idx], wavelength))
+            cStar.append([1,0])
+        fStar = np.array(fStar)
+        cStar = np.array(cStar)
+
         y1 = np.random.uniform(0,10000,nGalaxies)
         y2 = np.random.uniform(0,10000,nGalaxies)
         for idx in range(len(y1)):
-            galaxies.append(np.linspace(y1[idx],y2[idx],1000))
-        stars = np.array(stars)
-        galaxies = np.array(galaxies)
-        self.data = np.concatenate((stars,galaxies))
-        print(self.data)
+            fGalaxy.append(np.linspace(y1[idx],y2[idx],1000))
+            cGalaxy.append([0,1])
+        fGalaxy = np.array(fGalaxy)
+        cGalaxy = np.array(cGalaxy)
+
+        self.flux = np.concatenate((fStar,fGalaxy))
+        self.cls = np.concatenate((cStar,cGalaxy))
+        self.sample = np.random.random(nStars+nGalaxies) > 0.7
             
     def predict_class(self):
-        #Model parameters
-        W = tf.Variable([.3], dtype = tf.float32)
-        b = tf.Variable([-.3], dtype = tf.float32)
         
-        #Model input and output
-        x = tf.placeholder(tf.float32)
-        linear_model = W*x + b
-        y = tf.placeholder(tf.float32)
+        x = tf.placeholder(tf.float32, [None, len(self.flux[0])])
         
-        #Loss function (sum of the square error)
-        loss = tf.reduce_sum(tf.square(linear_model - y))
+        W = tf.Variable(np.zeros([len(self.flux[0]),len(self.cls[0])]), dtype = tf.float32)
+        b = tf.Variable(np.zeros(len(self.cls[0])), dtype = tf.float32)
         
-        #Optimizer
-        optimizer = tf.train.GradientDescentOptimizer(0.01)
-        train = optimizer.minimize(loss)
+        y = tf.nn.softmax(tf.matmul(x, W) + b)      
+        y_ = tf.placeholder(tf.float32, [None, len(self.cls[0])])
         
-        #Training data
-        x_train, x_test, y_train, y_test = train_test_split(self.df,test_size=0.5)
-        y_train = [0,-1,-2,-3]
-        data = {x: x_train, y: y_train}
+        x_train = self.flux[self.sample]
+        x_test = self.flux[[not s for s in self.sample]]
+        y_train = self.cls[self.sample]
+        y_test = self.cls[[not s for s in self.sample]]
         
-        #Training loop
-        sess = tf.Session()
-        init = tf.global_variables_initializer()
-        sess.run(init) #initialise model parameters
-        for i in range(1000):
-            sess.run(train, data)
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+        train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)           
         
-        curr_W, curr_b, curr_loss = sess.run([W, b, loss], data)
-        print('W: {}, b: {}, loss: {}'.format(curr_W, curr_b, curr_loss))
+        correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))        
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
+        
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(1000):
+                batch = np.random.random(len(x_train)) > 0.01
+                if i % 100 == 0:
+                    #train_accuracy = accuracy.eval(feed_dict={x: x_train[batch], y_: y_train[batch]})
+                    train_accuracy = accuracy.eval(feed_dict={x: x_test, y_: y_test})
+                    print('step %d, training accuracy %g' % (i, train_accuracy))
+                sess.run(train_step, feed_dict={x: x_train[batch], y_: y_train[batch]})
+                        
+            print(sess.run(accuracy, feed_dict={x: x_test, y_: y_test}))
         
     def blackbody(self, T, wavelength):
         ''' Models an ideal blackbody curve of a given temperature '''
@@ -63,4 +73,4 @@ class Neural_Net():
         
 if __name__ == "__main__":
     NN = Neural_Net()
-    NN.plot()
+    NN.predict_class()
